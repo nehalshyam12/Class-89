7154753035c7765918c0fa956018602aab93a01b
nehalshyam12 <75104500+nehalshyam12@users.noreply.github.com>
2021-08-24 08:34:50 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDrXbQSRsUwo1JY4n1wn0e0Lv_3GsRKJes",
+    authDomain: "storytelling-app-6df33.firebaseapp.com",
+    databaseURL: "https://storytelling-app-6df33-default-rtdb.firebaseio.com",
+    projectId: "storytelling-app-6df33",
+    storageBucket: "storytelling-app-6df33.appspot.com",
+    messagingSenderId: "628491063136",
+    appId: "1:628491063136:web:aaf177e42e8b4f97ce1805"
+  };
+
